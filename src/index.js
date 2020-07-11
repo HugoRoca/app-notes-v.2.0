caaced3f761e0d2085d126e60d8259f69ae4968e
HugoRoca <hugo.rock20@hotmail.com>
2020-07-10 22:38:08 -0500

    configuration src/index

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+// initializations
+const app = express()
+
+// settings
+app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}))
+app.set('view engine', '.hbs')
+
+// middleware
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extends:false }))
+app.use(express.json())
+
+// global variables
+app.use((req, res, next) => {
+  next()
+})
+
+// routes
+app.use((require('./routes/index')))
+app.use((require('./routes/authentication')))
+app.use('/links', (require('./routes/links')))
+
+// public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server is running in port', app.get('port'))
+})
