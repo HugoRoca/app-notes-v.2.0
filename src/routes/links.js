b18634dcfce2eebbf4e2ae654c6202b9cd09d975
HugoRoca <hugo.rock20@hotmail.com>
2020-07-11 23:45:59 -0500

    add views

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,4 +2,12 @@ const { Router } = require('express')
 const db = require('../database')
 const router = Router()
 
+router.get('/add', (req, res) => {
+  res.render('links/add')
+})
+
+router.post('/add', async (req, res) => {
+  res.send('received')
+})
+
 module.exports = router
