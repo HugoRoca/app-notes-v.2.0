ffc0047385d3b219b06c91e6322254de5e369f53
HugoRoca <hugo.rock20@hotmail.com>
2020-07-12 22:11:07 -0500

    Retrieving Data from Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,12 +7,17 @@ router.get('/add', (req, res) => {
 })
 
 router.post('/add', async (req, res) => {
-  const {title, url, description} = req.body
+  const { title, url, description } = req.body
   const newLink = {
     title, url, description
   }
   await db.query('INSERT INTO links set ?', [newLink])
-  res.send('received')
+  res.redirect('/links')
+})
+
+router.get('/', async (req, res) => {
+  const links = await db.query('SELECT * FROM links')
+  req.render('links/list', { links })
 })
 
 module.exports = router
