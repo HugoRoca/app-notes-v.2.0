3c167108429891d3dd657aff8a56d6bc96c328f1
HugoRoca <hugo.rock20@hotmail.com>
2020-07-12 22:34:28 -0500

    Updating Data from Mysql

--- src/routes/links.js
+++ src/routes/links.js
@@ -26,4 +26,20 @@ router.get('/delete/:id', async (req, res) => {
   res.redirect('/links')
 })
 
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const link = await db.query('SELECT * FROM links WHERE id = ?', [id])
+  res.render('links/edit', { link: link[0] })
+})
+
+router.post('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const { title, url, description } = req.body
+  const newLink = {
+    title, url, description
+  }
+  await db.query('UPDATE links SET ? WHERE id = ?', [newLink, id])
+  res.redirect('/links')
+})
+
 module.exports = router
